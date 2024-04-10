d896d938377a6d1eb2aeb2e75abaa1eb0c177fd9
TheSPICAL <youssefmasakni@gmail.com>
2024-04-10 16:09:54 +0100

    fix

--- slide_line/slide_line.h
+++ slide_line/slide_line.h
@@ -1,12 +1,16 @@
 #ifndef SLIDE_LINE_H
 #define SLIDE_LINE_H
 
+/* INCLUDED LIBRARIES */
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SLIDE_LEFT 0
-#define SLIDE_RIGHT 1
+/* DEFINITIONS */
+#define SLIDE_LEFT -5
+#define SLIDE_RIGHT 5
 
+/* FUNCTION PROTOTYPES */
 int slide_line(int *line, size_t size, int direction);
+void slide(int *line, int index, int placeholder);
 
 #endif
